a39540de219aeda1f1db516a31c333871d22f7c9
Kirill <ponur0kirill@gmail.com>
2019-11-04 18:34:08 +0300

    sync

--- project/main.py
+++ project/main.py
@@ -4,23 +4,24 @@ from numpy import pi
 from scipy import interpolate,integrate
 from tqdm import tqdm
 import water
-N = 2000
-M = 100
+N = 2048
+M = 256
 
 t = 0
 
 
 def plot_surface(x , y, t):
     fig,ax = plt.subplots(nrows = 1, ncols = 1)
-    surface = water.Surface(N=N,M=M)
+    surface = water.Surface(N=N,M=M,U10=5)
     x, y = np.meshgrid(x, y)
     z = surface.model([x,y],t)
     print(z.shape)
     from matplotlib.cm import winter
     plt.contourf(z,100,cmap=winter)
-    # plt.colorbar()
-    # # plt.ylabel(r'Y, \text{м}',fontsize=16)
-    # plt.xlabel(r'X, \text{м}',fontsize=16)
+    plt.colorbar()
+    plt.ylabel('Y, м',fontsize=16)
+    plt.xlabel('X, м',fontsize=16)
+    plt.savefig('water5.png', transparent=True, pdi=10**6,)
 
 x0 = np.linspace(0,200,200)
 y0 = x0
