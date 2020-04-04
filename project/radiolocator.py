3989d1275eda0410c4312932185a312b3fd4d615
Kirill <ponur0kirill@yandex.ru>
2020-04-05 00:54:19 +0300

    delete pulse_v

--- project/radiolocator.py
+++ project/radiolocator.py
@@ -5,19 +5,10 @@ from numpy import pi
 from scipy import interpolate,integrate
 from scipy.optimize import curve_fit
 from scipy.special import erf
+from scipy.optimize import anderson
+from scipy.optimize import root
 
 xrad = yrad = 0
-def ionsphere():
-    pass
-
-def troposphere_dry():
-    pass
-def troposphere_wet():
-    pass
-
-def spherical_earth():
-    pass
-
 
 def U10(sigma0):
     if sigma0 < 10.12:
@@ -30,18 +21,27 @@ def U10(sigma0):
         A = 0.01595
         B = 0.017215
 
-    return (np.exp(10**(0.21 + 0.1*sigma0)) - B)/A
+    return (np.exp(10**-(0.21 + 0.1*sigma0)) - B)/A
 
 class Radiolocator():
-    def __init__(self, h=1e6, xi=0, theta=1.5, c=3e8, sigma=1, angles_in='degrees'):
+    def __init__(self, h=1e6, xi=0.0, theta=1.5, c=3e8, sigma=1, 
+                    angles_in='degrees', pulse = np.array([None]), t = None):
+        self.R = 6370e3 # Радиус Земли в метрах
+        self.c = c # Скорость света в м/с
         if angles_in=='degrees':
             self.xi = np.deg2rad(xi) # Отклонение антенны в радианах
             self.theta = np.deg2rad(theta) # Ширина диаграммы направленности в радианах
-        
-        self.h = h # Высота орбиты в метрах
-        self.c = c # Скорость света в м/с
-        self.sigma_s = sigma # Дисперсия наклонов
-        self.R = 6370e3 # Радиус Земли в метрах
+            self.Gamma = self.gamma(self.theta)
+
+        if pulse.any() != None:
+            params = radiolocator.calc(t,pulse)
+            print('xi = {},\nh = {},\nsigma_s = {}'.format(params[0],params[1],params[2]))
+
+        else:
+            self.h = h # Высота орбиты в метрах
+            self.sigma_s = sigma # Дисперсия наклонов
+
+
         self.T = 3e-9 # Временное разрешение локатора
 
 
@@ -77,7 +77,7 @@ class Radiolocator():
     def pulse(self,t, dim = 1):
 
         self.dim = dim
-        gamma = self.gamma(self.theta)
+        gamma = self.Gamma
         delta = self.delta(gamma,self.xi,self.h)
         beta  = self.beta(gamma,self.xi,self.h)
 
@@ -102,10 +102,11 @@ class Radiolocator():
 
         return pulse
 
-    def pulse1(self, v, dim = 1):
+    def pulse_v(self, v, dim = 1):
 
         self.dim = dim
         gamma = self.gamma(self.theta)
+
         delta = self.delta(gamma,self.xi,self.h)
         beta  = self.beta(gamma,self.xi,self.h)
 
@@ -116,84 +117,61 @@ class Radiolocator():
         u = np.sqrt(2)*v/(alpha*sigma_c) - alpha*sigma_c/np.sqrt(2)
 
         A = self.A(gamma,self.xi)
-        print(A, sigma_c)
         pulse = A*np.exp(-v)*( 1 + erf(u) )
 
         return pulse
 
-    def pulse2(self,t, theta, xi, h, sigma_s):
+    def calc(self,t,pulse):
+        self.brown = lambda t,A,alpha,sigma:  A*np.exp(-alpha*(t - alpha/2 * sigma**2))*(1 + erf( (t - alpha*sigma**2)/(2**0.5*sigma))) 
+        
+        theta = np.deg2rad(1.5)
+        gamma = 2*np.sin(theta/2)**2/np.log(2)
+
+        popt = curve_fit(self.brown, 
+                            xdata=t,
+                            ydata=pulse,
+                            p0=[1,2e6,0],
+                            bounds=( (0,1e5,0), (2,4e6,np.inf) )
+                        ) [0]
+        print(popt)
+        c = self.c
+        R = self.R
+
+        tmp = np.log(popt[0])
+        if tmp > 0 :
+            xi = 0
+        else:
+            xi = np.arcsin( np.sqrt( -gamma/4 * tmp ))
 
 
-        xi = np.deg2rad(xi) # Отклонение антенны в радианах
-        theta = np.deg2rad(theta) # Ширина диаграммы направленности в радианах
-        gamma = self.gamma(theta)
-        delta = self.delta(gamma,xi,h)
-        beta  = self.beta(gamma,xi,h)
+        h0 = 4*c/gamma/popt[1]  * (np.cos(2*xi) - np.sin(2*xi)**2/gamma)
+        h  = -R/2 + np.sqrt(R**2 +4*h0*R)/2
 
-        alpha = self.alpha(beta,delta)
-
-        sigma_c = self.sigma_c(sigma_s)
-
-        u = self.u(t, alpha, sigma_c)
-        v = self.v(t, alpha, sigma_c)
-
-        A = self.A(gamma,xi)
-        pulse = A*np.exp(-v)*( 1 + erf(u) )
-
-        return pulse
+        sigma_s = (popt[2]**2 - (self.T*0.425) **2)*c**2/4
+        P = max(pulse) - min(pulse)
+        return np.rad2deg(xi), h, sigma_s, 10*np.log10(P)
 
 
 
-alpha = 2097955
-sigma = 6.7874e-9
-brown = lambda t,A,alpha,sigma,T:  A*np.exp(-alpha*(t - alpha/2 * sigma**2))*(1 + erf( (t - alpha*sigma**2)/(2**0.5*sigma))) + T
-ice = lambda t,A,tau,sigma,S:  A*np.exp(S*(t - tau))*(1 + erf( (t - tau)/(sigma)))
+
+radiolocator = Radiolocator(xi = 0.1, sigma=1)
 t = np.linspace(-5e-8, 2e-7, 256)
-v = np.linspace(-0.1,0.5,1000)
-radiolocator = Radiolocator()
-plt.figure(1)
-pulse = radiolocator.pulse(t,dim=1) + np.random.uniform(-0.3,0.3,size=t.size)
+
+pulse = radiolocator.pulse(t)     
+A = max(pulse) - min(pulse)
+err = 0.01
+
+plt.plot(t,pulse,label='теория')
+pulse +=  np.random.uniform(-err*A,err*A,size=t.size)
+plt.plot(t,pulse,':',label='теория + шум')
+
+params = radiolocator.calc(t=t,pulse=pulse)
 
 
-p, pcov = curve_fit(brown, xdata=t,ydata=pulse,p0=[0,alpha*0.01,0,0])
-# p1, pcov = curve_fit(ice, xdata=t,ydata=pulse,p0=[1,(alpha*sigma)**2,0,alpha])
-# print(p1)
-# print(popt)
-
-
-
-# plt.plot(t,brown(t,p[0],p[1],p[2]))
-plt.plot(t,pulse,label='теория + белый шум')
-t = np.linspace(-5e-8, 2e-7, 512)
-pulse0 = brown(t,p[0],p[1],p[2],p[3])
-sigma0 = max(pulse0) - min(pulse0)
-print(sigma0)
-plt.plot(t,pulse0,label='аппроксимация')
-# plt.plot(t,ice(t,p1[0],p1[1],p1[2],p1[3]))
-plt.plot(t,brown(t,1,alpha,sigma,0),label='теория')
+radiolocator = Radiolocator(xi = params[0], h = params[1], sigma = params[2])
+pulse = radiolocator.pulse(t)     
+plt.plot(t,pulse,label='аппроксимация')
 plt.legend()
+plt.title( 'xi = {:.2},\nh = {:.1},\nsigma_s = {:.3}, sigma0 = {:.2}'.format(params[0],params[1],params[2],params[3]))
 plt.show()
-
-
-
-# M = 256
-
-
-# N = 256
-# x0 = np.linspace(0,200,50)
-# y0 = x0
-# t = 0
-
-
-# fig,ax = plt.subplots(nrows = 1, ncols = 1)
-# surface = water.Surface(N=N,M=M,U10=5,wind= np.pi/6)
-# x, y = np.meshgrid(x0, y0)
-# z = surface.model([x,y],t)
-# print(z.shape)
-# from matplotlib.cm import winter
-# plt.contourf(x,y,z,levels=100,cmap=winter)
-# plt.colorbar()
-
-# plt.ylabel('Y, м',fontsize=16)
-# plt.xlabel('X, м',fontsize=16)
-# plt.show()
+print(U10(params[-1]))
